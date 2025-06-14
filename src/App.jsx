da4fd2ead2b6cdca6b45472b802891b83f7fd586
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-06-14 17:31:15 +0300

    Refactor loading spinner in App and LazyLoading components to apply rotation for enhanced visual effect.

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const BookingConfirmation = lazy(() => import('./pages/BookingConfirmation'));
 function App() {
   return (
     <div className='flex items-center justify-center h-screen'>
-    <div className="w-[150px] h-20">
-      <span className="loading loading-infinity text-[#9e8e78] w-full "></span>
+    <div className="w-[150px] h-20  rotate-90 ">
+      <span className="loading loading-infinity text-[#9e8e78] w-full  "></span>
     </div>
         </div>
    
--- src/components/LazyLoading/LazyLoading.jsx
+++ src/components/LazyLoading/LazyLoading.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function LazyLoading() {
   return (
     <div className='flex items-center justify-center h-screen'>
-<div className="w-[150px] h-20">
-  <span className="loading loading-infinity text-[#9e8e78] w-full "></span>
-</div>
+    <div className="w-[150px] h-20  rotate-90 ">
+      <span className="loading loading-infinity text-[#9e8e78] w-full  "></span>
     </div>
+        </div>
   )
 }
