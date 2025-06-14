cd73869834644b78a435c6b5b8e77ba10599f50e
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-06-14 17:35:56 +0300

    Refactor loading spinner in LazyLoading component to ensure consistent styling with App component for improved visual alignment.

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const BookingConfirmation = lazy(() => import('./pages/BookingConfirmation'));
 function App() {
   return (
     <div className='flex items-center justify-center h-screen'>
-    <div className="w-[150px] h-20  rotate-90 ">
+    <div className="w-[150px] h-20  rotate-90   flex items-center justify-center">
       <span className="loading loading-infinity text-[#9e8e78] w-full  "></span>
     </div>
         </div>
